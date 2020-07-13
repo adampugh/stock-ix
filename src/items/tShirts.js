0877f62610817acece0ee1cccb7bebc3fb4e99eb
Adam <adampugh@zoho.com>
2020-07-13 11:24:17 +0100

    update

--- src/items/tShirts.js
+++ src/items/tShirts.js
@@ -1,10 +1,12 @@
 import SupremeVampirella from '../images/tshirts/supremexvampirella.jpg'
+import SupremeHellraiser from '../images/tshirts/supremexhellraiser.jpg'
+import SupremeRubberJohnny from '../images/tshirts/supremexrubberjohnny.jpg'
 
 
 const tshirts = [
     { name: 'Supreme x Vampirella', img: SupremeVampirella, category: 'tshirts' },
-    { name: '', img: '', category: 'tshirts' },
-    { name: '', img: '', category: 'tshirts' },
+    { name: 'Supreme x Hellraiser', img: SupremeHellraiser, category: 'tshirts' },
+    { name: 'Supreme x Rubber Johnny', img: SupremeRubberJohnny, category: 'tshirts' },
     { name: '', img: '', category: 'tshirts' },
     { name: '', img: '', category: 'tshirts' },
     { name: '', img: '', category: 'tshirts' },
