49652498981f534fcd7a5218f67135aa2811a944
Adam <adampugh@zoho.com>
2020-07-13 23:52:36 +0100

    update

--- src/components/item.js
+++ src/components/item.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import QuestionMark from '../images/questionmark.jpg'
 
 const Item = ({ item }) => {
     const { name, img } = item
+    const src = img ? img : QuestionMark
 
     return (
-        <div className="item">
-            <img src={img} alt={name} />
-        </div>   
+        <div className="item__background">
+            <div className="item">
+                <img src={src} alt={name} />
+            </div>
+            <div className="item__hover">
+                <p className="item__name">{name}</p>
+            </div>
+        </div>
     )
 }
 
--- src/items/tShirts.js
+++ src/items/tShirts.js
@@ -2,14 +2,20 @@ import SupremeVampirella from '../images/tshirts/supremexvampirella.jpg'
 import SupremeHellraiser from '../images/tshirts/supremexhellraiser.jpg'
 import SupremeRubberJohnny from '../images/tshirts/supremexrubberjohnny.jpg'
 
+import MastermindTimberland from '../images/tshirts/mastermindxtimberland.jpg'
+
+import PalaceWiseUp from '../images/tshirts/palacewiseup.jpg'
+import PalaceHeshMitFresh from '../images/tshirts/palaceheshmitfresh.jpg'
+
 
 const tshirts = [
     { name: 'Supreme x Vampirella', img: SupremeVampirella, category: 'tshirts' },
     { name: 'Supreme x Hellraiser', img: SupremeHellraiser, category: 'tshirts' },
     { name: 'Supreme x Rubber Johnny', img: SupremeRubberJohnny, category: 'tshirts' },
-    { name: '', img: '', category: 'tshirts' },
-    { name: '', img: '', category: 'tshirts' },
-    { name: '', img: '', category: 'tshirts' },
+    { name: 'Mastermind x Timberland', img: MastermindTimberland, category: 'tshirts' },
+    { name: 'Palace Wise Up', img: PalaceWiseUp, category: 'tshirts' },
+    { name: 'Palace Hesh Mit Fresh', img: PalaceHeshMitFresh, category: 'tshirts' },
+    
     { name: '', img: '', category: 'tshirts' },
     { name: '', img: '', category: 'tshirts' },
     { name: '', img: '', category: 'tshirts' },
