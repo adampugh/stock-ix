cce585c889c956d1d5f2d2d24055ab7290075c17
Adam <adampugh@zoho.com>
2020-07-21 16:41:29 +0100

    refactor to use hooks

--- src/components/mainContent.js
+++ src/components/mainContent.js
@@ -1,40 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import Sidebar from './sidebar';
 import Items from './items';
 
 import items from '../items/items';
 
-class MainContent extends Component {
-    state = {
-        selectedItems: []
-    }
+const MainContent = () => {
+    const [ selectedItems, setSelectedItems ] = useState(items)
 
-    componentDidMount() {
-        this.setState(() => ({
-            selectedItems: items
-        }))
-    }
-
-    filterItems = e => {
+    const filterItems = e => {
         const category = e.target.innerHTML
         const categoryFormatted = category.toLowerCase().replace(/-/, '')
         const updatedCategory = categoryFormatted === 'all' ? items : items.filter(item => item.category === categoryFormatted)
-        this.setState(() => ({
-            selectedItems: updatedCategory
-        }))
+        setSelectedItems(updatedCategory)
     }
 
-    render() {
-        const { selectedItems } = this.state;
-
-        return (
-            <div className="main-content">
-                <Sidebar filterItems={this.filterItems} />
-                <Items items={selectedItems} />
-            </div>
-        )
-    }
+    return (
+        <div className="main-content">
+            <Sidebar filterItems={filterItems} />
+            <Items items={selectedItems} />
+        </div>
+    )
 }
 
 export default MainContent;
